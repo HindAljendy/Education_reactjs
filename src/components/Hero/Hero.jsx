c28a25d6e123617f83bd03d0a7aa79a9d12ea152
HindAljendy <142920133+HindAljendy@users.noreply.github.com>
2025-06-18 01:28:43 +0800

    update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         /* when use react-scroll : add values target in properties name or id or unique className */
         <div className='hero container' name="home">
             <div className='hero-text'>
-                <h1>We Ensure better education for a better world</h1>
+                <h1>wwwwwwwwwwWe Ensure better education for a better world</h1>
                 <p>Our cutting-edge curriculum is designed to empower students with the knowledge,
                     skills, and experiences needed to excel in the dynamic field of education
                 </p>
